6fa7f52a95277f8dbb46b7f160f73de0191a19c3
KubaGo <jakub.golcgdy@gmail.com>
2024-04-24 21:09:02 +0200

    Add Dobro methods

--- src/main/java/com/kubago/jakzycpaniepremierze/models/Postac.java
+++ src/main/java/com/kubago/jakzycpaniepremierze/models/Postac.java
@@ -2,14 +2,15 @@ package com.kubago.jakzycpaniepremierze.models;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Postac {
     private String name;
     private Path path_to_image;
     private int dostepneSrodki;
     private Praca praca;
-    private ArrayList<Dobro> listaDobr;
-    private ArrayList<Pozyczka> listaPozyczek;
+    private HashMap<String,Dobro> listaDobr;
+    private HashMap<String,Pozyczka> listaPozyczek;
     private Lotek lotek;
 
 
@@ -22,6 +23,20 @@ public class Postac {
     }
     public void strataPracy(){
         this.praca = praca.wybierz_prace(Prace.BEZROBOTNY);
+    }
+    public void kupDobro(Dobro dobro) throws Exception {
+        if (this.dostepneSrodki < dobro.getCena()){
+            throw new Exception("Nie można kupić. Niewystarczająca ilość środków.");
+        }
+        this.dostepneSrodki =- dobro.getCena();
+        listaDobr.put(dobro.getNazwa(), dobro);
+    }
+    public void sprzedajDobro(String nazwa){
+        if (!listaDobr.containsKey(nazwa)){
+            throw new IllegalArgumentException("Ta postać nie ma takiego dobra.");
+        }
+        Dobro dobro = listaDobr.remove(nazwa);
+        this.dostepneSrodki += dobro.getCena_sprzedazy();
     }
 
 }
