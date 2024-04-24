8140fcad8ae4347466aa0b81ab60bf3cc60e8993
KubaGo <jakub.golcgdy@gmail.com>
2024-04-24 20:45:51 +0200

    Add base constructor

--- src/main/java/com/kubago/jakzycpaniepremierze/models/Postac.java
+++ src/main/java/com/kubago/jakzycpaniepremierze/models/Postac.java
@@ -1,4 +1,21 @@
 package com.kubago.jakzycpaniepremierze.models;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class Postac {
+    private String name;
+    private Path path_to_image;
+    private int dostepneSrodki;
+    private Praca praca;
+    private ArrayList<Dobro> listaDobr;
+    private ArrayList<Pozyczka> listaPozyczek;
+    private Lotek lotek;
+
+
+    public Postac(String name, Path path_to_image) {
+        this.name = name;
+        this.path_to_image = path_to_image;
+    }
+
 }
