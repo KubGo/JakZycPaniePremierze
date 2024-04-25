26c82c0217b05c4d712d3ac800b99c6dc1d27040
KubaGo <jakub.golcgdy@gmail.com>
2024-04-25 22:24:47 +0200

    Rename classes to JZPP

--- src/main/java/com/kubago/jakzycpaniepremierze/JZPPController.java
+++ src/main/java/com/kubago/jakzycpaniepremierze/JZPPController.java
@@ -3,7 +3,7 @@ package com.kubago.jakzycpaniepremierze;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class JZPPController {
     @FXML
     private Label welcomeText;
 
