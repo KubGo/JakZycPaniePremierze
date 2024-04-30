b314cb96121915fc1dfd7c0c224b54a2371dd61a
KubaGo <jakub.golcgdy@gmail.com>
2024-04-30 22:21:47 +0200

    Update main app

--- src/main/java/com/kubago/jakzycpaniepremierze/JZPPApplication.java
+++ src/main/java/com/kubago/jakzycpaniepremierze/JZPPApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class JZPPApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(JZPPApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(JZPPApplication.class.getResource("start-menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
