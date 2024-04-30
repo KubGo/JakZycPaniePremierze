21bb3b3066bb32c7b8caf5006f93545902bf7328
KubaGo <jakub.golcgdy@gmail.com>
2024-04-30 22:15:36 +0200

    Add chosing for urzednik

--- src/main/java/com/kubago/jakzycpaniepremierze/StartMenuController.java
+++ src/main/java/com/kubago/jakzycpaniepremierze/StartMenuController.java
@@ -3,9 +3,9 @@ package com.kubago.jakzycpaniepremierze;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -30,20 +30,37 @@ public class StartMenuController implements Initializable {
     public void ustawGraczy(ActionEvent event){
         Integer liczbaGraczy = wybierzLiczbeGraczy.getValue();
         gracze.getChildren().clear();
+        ToggleGroup urzednik = new ToggleGroup();
 
         for (int i = 1; i <= liczbaGraczy; i++){
-            HBox gracz = new HBox();
+            GridPane gracz = new GridPane();
+            gracz.setHgap(10);
             gracz.getChildren().clear();
             gracz.setId("gracz_" + i);
             Label playerData = new Label("Gracz nr: " + i + ":");
-            gracz.getChildren().add(playerData);
+            gracz.add(playerData, 0, 0);
             TextField name = new TextField();
             name.setId("name_" + i);
-            gracz.getChildren().add(name);
-
+            gracz.add(name, 1, 0);
+            RadioButton isUrzednik = new RadioButton();
+            isUrzednik.setId("urzednik_" + i);
+            isUrzednik.setToggleGroup(urzednik);
+            gracz.add(isUrzednik, 2,0);
             gracze.getChildren().add(gracz);
 
         }
+        Label baseUrzednikLabel = new Label("Wybierz urzędnika zaznaczając gracza. " +
+                "Jeżeli brak znaznacz ten przycisk:");
+        baseUrzednikLabel.setWrapText(true);
+        RadioButton noUrzednik = new RadioButton();
+        noUrzednik.setToggleGroup(urzednik);
+        noUrzednik.setId("noneUrzednik");
+        noUrzednik.setSelected(true);
+        GridPane noneUrzednik = new GridPane();
+        noneUrzednik.add(baseUrzednikLabel, 0,0,2,2);
+        noneUrzednik.add(noUrzednik, 2, 0, 1, 2);
+        noneUrzednik.setHgap(10);
+        gracze.getChildren().add(noneUrzednik);
     }
 
 }
